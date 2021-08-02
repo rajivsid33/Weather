5f1896f227ad1afe5fdae7c02c87ab24f8bbfdfc
rajivsid33 <atsrajiv@gmail.com>
2021-08-02 18:31:55 +0530

    Changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React, { useState,useEffect } from "react";
-import Sidebar from "./components/Sidebar";
+import Sidebar from "./components/Sidebar"
 
 function App() {
 
